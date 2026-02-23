a4328130ce272eb5727e974346f2eceb76f7e3fe
Gian Pierre <giancordova9@gmail.com>
2026-02-22 20:20:41 -0500

    Actualizando mi título con magia automática

--- src/SobreMi.jsx
+++ src/SobreMi.jsx
@@ -14,7 +14,15 @@ function SobreMi({ idioma }) { /* <-- Recibimos el idioma */
           <p>{t.parrafo1}</p>
           <p>{t.parrafo2}</p>
         </div>
-
+{/* Asegúrate de que el href coincida exactamente con el nombre de tu PDF */}
+<a 
+  href="/CV_Adrian_Ruiz.pdf" 
+  download="CV_Adrian_Ruiz.pdf" 
+  className="btn-primario" 
+  style={{ marginTop: '20px', display: 'inline-block' }}
+>
+  📄 Descargar Mi CV
+</a>
         <div className="habilidades-sobremi">
           <h3>{t.tituloStack}</h3>
           <div className="grid-habilidades">
